8b6b790eb3f2fe8a0a38053522619758c2c7fe9d
ict.pcto <ict.pcto@CL119>
2026-06-25 09:33:12 +0200

    Aggiungere i file di progetto.

---
+++ EsACaso/Form1.Designer.cs
@@ -0,0 +1,172 @@
+﻿namespace EsACaso
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnDebloat = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.rtbDispositivo = new System.Windows.Forms.RichTextBox();
+            this.btnRiavvia = new System.Windows.Forms.Button();
+            this.btnInfo = new System.Windows.Forms.Button();
+            this.btnImmagine = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnDebloat
+            // 
+            this.btnDebloat.Location = new System.Drawing.Point(12, 55);
+            this.btnDebloat.Name = "btnDebloat";
+            this.btnDebloat.Size = new System.Drawing.Size(776, 36);
+            this.btnDebloat.TabIndex = 0;
+            this.btnDebloat.Text = "DEBLOAT NOW";
+            this.btnDebloat.UseVisualStyleBackColor = true;
+            this.btnDebloat.Click += new System.EventHandler(this.btnDebloat_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Ravie", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(243, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(319, 30);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "CHRIS TITUS DEBLOAT";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.richTextBox1.Location = new System.Drawing.Point(12, 147);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(319, 144);
+            this.richTextBox1.TabIndex = 2;
+            this.richTextBox1.Text = "";
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Ravie", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(7, 113);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(303, 26);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Componenti di sistema";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Ravie", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(635, 113);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(153, 26);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Dispositivo";
+            // 
+            // rtbDispositivo
+            // 
+            this.rtbDispositivo.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rtbDispositivo.Location = new System.Drawing.Point(469, 147);
+            this.rtbDispositivo.Name = "rtbDispositivo";
+            this.rtbDispositivo.Size = new System.Drawing.Size(319, 144);
+            this.rtbDispositivo.TabIndex = 6;
+            this.rtbDispositivo.Text = "";
+            // 
+            // btnRiavvia
+            // 
+            this.btnRiavvia.Location = new System.Drawing.Point(12, 313);
+            this.btnRiavvia.Name = "btnRiavvia";
+            this.btnRiavvia.Size = new System.Drawing.Size(776, 36);
+            this.btnRiavvia.TabIndex = 7;
+            this.btnRiavvia.Text = "RIAVVIA IL PC ";
+            this.btnRiavvia.UseVisualStyleBackColor = true;
+            this.btnRiavvia.Click += new System.EventHandler(this.btnRiavvia_Click);
+            // 
+            // btnInfo
+            // 
+            this.btnInfo.Location = new System.Drawing.Point(760, 17);
+            this.btnInfo.Name = "btnInfo";
+            this.btnInfo.Size = new System.Drawing.Size(28, 23);
+            this.btnInfo.TabIndex = 8;
+            this.btnInfo.Text = "?";
+            this.btnInfo.UseVisualStyleBackColor = true;
+            this.btnInfo.Click += new System.EventHandler(this.btnInfo_Click);
+            // 
+            // btnImmagine
+            // 
+            this.btnImmagine.Location = new System.Drawing.Point(469, 255);
+            this.btnImmagine.Name = "btnImmagine";
+            this.btnImmagine.Size = new System.Drawing.Size(319, 36);
+            this.btnImmagine.TabIndex = 9;
+            this.btnImmagine.Text = "VISUALIZZA IMMAGINE SUL WEB";
+            this.btnImmagine.UseVisualStyleBackColor = true;
+            this.btnImmagine.Click += new System.EventHandler(this.btnImmagine_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 360);
+            this.Controls.Add(this.btnImmagine);
+            this.Controls.Add(this.btnInfo);
+            this.Controls.Add(this.btnRiavvia);
+            this.Controls.Add(this.rtbDispositivo);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnDebloat);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnDebloat;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.RichTextBox rtbDispositivo;
+        private System.Windows.Forms.Button btnRiavvia;
+        private System.Windows.Forms.Button btnInfo;
+        private System.Windows.Forms.Button btnImmagine;
+    }
+}
+
---
+++ EsACaso/Form1.cs
@@ -0,0 +1,186 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Management;
+using Microsoft.Web.WebView2.WinForms;
+
+namespace EsACaso
+{
+    public partial class Form1 : Form
+    {
+        private string _serial = "";
+        private string _model = "";
+        private string _manufacturer = "";
+
+        public Form1()
+        {
+            InitializeComponent();
+            (_serial, _model, _manufacturer) = GetComputerInfo();
+            CaricaInfoSistema();
+            CaricaInfoHardware();
+            Timer timer = new Timer();
+            timer.Interval = 1000;
+            timer.Tick += (s, e) => CaricaInfoSistema();
+            timer.Start();
+        }
+
+        private async void btnDebloat_Click(object sender, EventArgs e)
+        {
+            btnDebloat.Enabled = false;
+            try
+            {
+                await RunScript("https://christitus.com/win", "christitus_win.ps1");
+                MessageBox.Show("Tutto completato!\nRiavvia il sistema per fare in modo che i cambiamenti abbiano effetto.", "Done", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (Exception ex) { MessageBox.Show("Controlla la connessione a internet", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error); }
+            btnDebloat.Enabled = true;
+        }
+
+        private async Task RunScript(string url, string fileName)
+        {
+            string tempFile = Path.Combine(Path.GetTempPath(), fileName);
+            using (var client = new System.Net.Http.HttpClient())
+            {
+                string scriptContent = await client.GetStringAsync(url);
+                File.WriteAllText(tempFile, scriptContent, Encoding.UTF8);
+            }
+            var process = new Process();
+            process.StartInfo.FileName = "powershell.exe";
+            process.StartInfo.Arguments = $"-NoProfile -ExecutionPolicy Bypass -File \"{tempFile}\"";
+            process.StartInfo.UseShellExecute = true;
+            process.StartInfo.Verb = "runas";
+            process.Start();
+            await Task.Run(() => process.WaitForExit());
+            File.Delete(tempFile);
+        }
+
+        private void CaricaInfoSistema()
+        {
+            string os = Environment.OSVersion.VersionString;
+
+            MEMORYSTATUSEX mem = new MEMORYSTATUSEX();
+            GlobalMemoryStatusEx(mem);
+            DriveInfo drive = new DriveInfo("C");
+
+            richTextBox1.Clear();
+            richTextBox1.AppendText($"OS:       {os}\n");
+            richTextBox1.AppendText($"CPU core: {Environment.ProcessorCount}\n");
+            richTextBox1.AppendText($"RAM:      {mem.ullAvailPhys / 1024 / 1024} MB liberi / {mem.ullTotalPhys / 1024 / 1024} MB totali\n");
+            richTextBox1.AppendText($"Disco C:  {drive.AvailableFreeSpace / 1024 / 1024 / 1024} GB liberi / {drive.TotalSize / 1024 / 1024 / 1024} GB totali\n");
+            richTextBox1.AppendText($"PC name:  {Environment.MachineName}\n");
+            richTextBox1.AppendText($"Utente:   {Environment.UserName}\n");
+            richTextBox1.ReadOnly = true;
+        }
+
+        private void CaricaInfoHardware()
+        {
+            rtbDispositivo.Clear();
+            rtbDispositivo.AppendText($"Produttore: {_manufacturer}\n");
+            rtbDispositivo.AppendText($"Modello:    {_model}\n");
+            rtbDispositivo.AppendText($"Seriale:    {_serial}\n");
+            rtbDispositivo.ReadOnly = true;
+        }
+
+        private (string serial, string model, string manufacturer) GetComputerInfo()
+        {
+            string serial = "N/A";
+            string model = "N/A";
+            string manufacturer = "N/A";
+
+            using (var s1 = new ManagementObjectSearcher("SELECT SerialNumber FROM Win32_BIOS"))
+                foreach (var obj in s1.Get())
+                    serial = obj["SerialNumber"]?.ToString() ?? "N/A";
+
+            using (var s2 = new ManagementObjectSearcher("SELECT Model, Manufacturer FROM Win32_ComputerSystem"))
+                foreach (var obj in s2.Get())
+                {
+                    model = obj["Model"]?.ToString() ?? "N/A";
+                    manufacturer = obj["Manufacturer"]?.ToString() ?? "N/A";
+                }
+
+            return (serial, model, manufacturer);
+        }
+
+        private string GetSupportUrl(string model, string serial, string manufacturer)
+        {
+            string m = manufacturer.ToLower();
+
+            if (m.Contains("dell"))
+                return $"https://www.dell.com/support/home/it-it/product-support/servicetag/{Uri.EscapeDataString(serial)}";
+
+            if (m.Contains("lenovo"))
+                return $"https://pcsupport.lenovo.com/it/it/products/{Uri.EscapeDataString(model)}";
+
+            if (m.Contains("hp") || m.Contains("hewlett"))
+                return $"https://support.hp.com/it-it/product/details/{Uri.EscapeDataString(serial)}";
+
+            if (m.Contains("asus"))
+                return $"https://www.asus.com/it/support/?ModelName={Uri.EscapeDataString(model)}";
+
+            if (m.Contains("acer"))
+                return $"https://www.acer.com/it/support/?sn={Uri.EscapeDataString(serial)}";
+
+            if (m.Contains("microsoft"))
+                return $"https://support.microsoft.com/it-it/surface";
+
+            return $"https://www.bing.com/search?q={Uri.EscapeDataString(manufacturer + " " + model + " support")}";
+        }
+
+        private void btnImmagine_Click(object sender, EventArgs e)
+        {
+            string url = GetSupportUrl(_model, _serial, _manufacturer);
+
+            Form form2 = new Form();
+            form2.Text = $"Supporto PC - {_manufacturer} {_model}";
+            form2.Size = new System.Drawing.Size(1100, 700);
+            form2.StartPosition = FormStartPosition.CenterScreen;
+
+            var webView = new WebView2();
+            webView.Dock = DockStyle.Fill;
+            form2.Controls.Add(webView);
+
+            webView.CoreWebView2InitializationCompleted += (s, e2) =>
+            {
+                webView.CoreWebView2.Navigate(url);
+            };
+
+            form2.Load += async (s, e2) =>
+            {
+                await webView.EnsureCoreWebView2Async();
+            };
+
+            form2.Show();
+        }
+
+        [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Auto)]
+        private class MEMORYSTATUSEX
+        {
+            public uint dwLength = (uint)Marshal.SizeOf(typeof(MEMORYSTATUSEX));
+            public uint dwMemoryLoad;
+            public ulong ullTotalPhys;
+            public ulong ullAvailPhys;
+            public ulong ullTotalPageFile;
+            public ulong ullAvailPageFile;
+            public ulong ullTotalVirtual;
+            public ulong ullAvailVirtual;
+            public ulong ullAvailExtendedVirtual;
+        }
+
+        [DllImport("kernel32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern bool GlobalMemoryStatusEx([In, Out] MEMORYSTATUSEX lpBuffer);
+
+        private void btnRiavvia_Click(object sender, EventArgs e)
+        {
+            Process.Start("shutdown.exe", "/r /t 0");
+        }
+
+        private void btnInfo_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("EsACaso v1.0\n\nQuesto programma raccoglie informazioni sul sistema e sull'hardware, e può eseguire uno script PowerShell remoto per configurare il sistema.\n\nSviluppato da Miner.", "Informazioni", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+    }
+}
